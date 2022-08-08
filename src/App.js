7c8797bfba8a453f4c65410791fa21a945ff8da8
Kelvin Anderson <kelvinanders17@gmail.com>
2022-08-08 09:19:11 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [dark, setDark] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: dark ? 'black': 'white'}}>
+      <p style={{color: dark ? 'white': 'black'}}>Hai, ini contoh react hooks !</p>
+      <button onClick={()=> setDark(!dark)}>Set Theme {dark? 'white': 'black'}</button>
     </div>
   );
 }
